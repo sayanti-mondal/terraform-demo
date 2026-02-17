a7f88299ba320327d2bf2d6c490709856bb73f89
sayanti-mondal <110446789+sayanti-mondal@users.noreply.github.com>
2026-02-17 15:07:18 +0530

    Create variables.tf

---
+++ ec2/variables.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+  description = "AMI ID to use for the EC2 instance"
+  type        = string
+  default     = "ami-019715e0d74f695be"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
